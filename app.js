ae7ae803cc3de7f47e63b8a64284e956f5c46e14
Luiz Otávio Rauber <142830693+luizotaviorauber@users.noreply.github.com>
2023-09-13 14:50:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteado = []
-let numeroLimite = 10
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -7,7 +7,7 @@ let tentativas = 1;
 function exibirTextoNaTela (tag, texto){
     let campo = document.querySelector(tag)
     campo.innerHTML = texto
-    responsiveVoice.speak (texto, 'Brazilian Portuguese Female', {rate:1.2});
+    //responsiveVoice.speak (texto, 'Brazilian Portuguese Female', {rate:1.2});
 
 }
 
@@ -15,7 +15,7 @@ exibirMensagemInicial()
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 
@@ -52,7 +52,7 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaNumerosSorteado.push(numeroEscolhido);
-        console.log(listaNumerosSorteado);
+        //console.log(listaNumerosSorteado);
         return numeroEscolhido;
         
     }
